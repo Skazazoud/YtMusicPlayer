8c4630e61bcba853a0895af8640cfe7241432b95
DesktopSka <matthieu.andre@epitech.eu>
2026-07-07 22:38:53 +0200

    Add setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using Microsoft.Web.WebView2.Core;
 using YtMusicPlayer.Services;
 
 namespace YtMusicPlayer
@@ -10,6 +12,23 @@ namespace YtMusicPlayer
         [STAThread]
         static void Main()
         {
+            if (!IsWebView2RuntimeAvailable())
+            {
+                MessageBox.Show(
+                    "YT Music Player needs the Microsoft Edge WebView2 Runtime, which isn't installed on this PC.\n\n" +
+                    "It's a small, free download from Microsoft - most Windows 10/11 PCs already have it since it ships with Edge. " +
+                    "Your browser will open to the download page now.",
+                    "WebView2 Runtime required",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning);
+
+                Process.Start(new ProcessStartInfo("https://developer.microsoft.com/en-us/microsoft-edge/webview2/")
+                {
+                    UseShellExecute = true
+                });
+                return;
+            }
+
             var singleInstance = new SingleInstanceService();
             if (!singleInstance.IsPrimaryInstance)
             {
@@ -24,6 +43,19 @@ namespace YtMusicPlayer
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new PlayerForm(singleInstance));
+        }
+
+        private static bool IsWebView2RuntimeAvailable()
+        {
+            try
+            {
+                CoreWebView2Environment.GetAvailableBrowserVersionString(null);
+                return true;
+            }
+            catch (WebView2RuntimeNotFoundException)
+            {
+                return false;
+            }
         }
     }
 }
